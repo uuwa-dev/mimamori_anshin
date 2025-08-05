1a30d5548fb53370276eabafe139948b367127fd
uuwa-dev <yokoi@uuwa.net>
2025-08-06 00:33:13 +0900

    iosビルドできるよう調整

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,12 +3,14 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class SplashScreen extends HookWidget {
+
+class SplashScreen extends HookConsumerWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     useEffect(() {
       final timer = Timer(const Duration(seconds: 2), () {
         context.go('/roleSelection');
